0e610aa3c60a7529b29f621db0c8f0b228f1aae4
Prue Singh <pruesinghnagra@gmail.com>
2025-06-02 12:16:15 +1200

    Remove listen and let vercel listen

--- index.ts
+++ index.ts
@@ -15,10 +15,4 @@ app.register(cors, {
 
 app.register(spotifyRoutes);
 
-app.listen({ port: 3001 }, (err, address) => {
-    if (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-    app.log.info(`Server listening at ${address}`);
-});
+export default app;
