fb885f67edf05ac5b3b0f75318b971e676b82426
Prue Singh <pruesinghnagra@gmail.com>
2025-07-04 01:56:35 +1200

    add debug route

--- api/essays/test.ts
+++
@@ -1,7 +0,0 @@
-// api/essays/test.ts
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json({ message: "Test route is working!" });
-}
