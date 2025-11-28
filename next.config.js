245c044e35c900bb603ec9c8e8c9091cdce956ed
Prue Singh <pruesinghnagra@gmail.com>
2025-11-29 00:11:34 +1300

    Schema and config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://music-lamp-backend.vercel.app/",
+  ],
 };
 
 module.exports = nextConfig;
