63d8cc0c02be424fff368361e5f8739f0aca9150
Prue Singh <pruesinghnagra@gmail.com>
2026-02-24 16:17:36 +1300

    was too hasty - but removed unused import and variable

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,9 @@
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import AdminDashboard from "./AdminDashboard";
 
 export default async function AdminPage() {
   // Cookies
   const cookieStore = await cookies();
-  const session = cookieStore.get("admin_session");
 
   return <AdminDashboard />;
 }
